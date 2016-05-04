ceaf55511e3860d611f1b7f647bf8346f95bd6a1
vinicius-insper <viniciusbf1@al.insper.edu.br>
2016-05-04 16:32:36 -0300

    Ao inves de fazer o dicionario criei uma classe

--- Banco de Dados.py
+++ Banco de Dados.py
@@ -1,6 +1,7 @@
-menu = {'000':{'Coca Cola':3,50}, '001':{'Guarana':3,50}, '002':{'Citrus':3,50}, '003':{'Agua':3,00}, '004':{'Sucos':4,50},
-'005':{'Batata Frita':8,00}, '006':{'Mandioca':9,00}, '007':{'Polenta':8,50}, '008':{'X-Burguer':9,50}, '009':{'Burguer':9,00}, 
-'010':{'X-Frango':10,50}, '011':{'Frango':10,00}, '012':{'Ovo':1,50}, '013':{'Bacon':1,50}, '014':{'Salada':1,50}, '015':{'MilkShake':6,50}}
-}
+class Banco de Dados():
+	def __init__(self):
+		self.menu = {'000':{'Coca Cola':3,50}, '001':{'Guarana':3,50}, '002':{'Citrus':3,50}, '003':{'Agua':3,00}, '004':{'Sucos':4,50},
+		'005':{'Batata Frita':8,00}, '006':{'Mandioca':9,00}, '007':{'Polenta':8,50}, '008':{'X-Burguer':9,50}, '009':{'Burguer':9,00}, 
+		'010':{'X-Frango':10,50}, '011':{'Frango':10,00}, '012':{'Ovo':1,50}, '013':{'Bacon':1,50}, '014':{'Salada':1,50}, '015':{'MilkShake':6,50}}
 
 
