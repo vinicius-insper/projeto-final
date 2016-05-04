845f77d6c7af88a25677e289eccc5bc924cf1757
vinicius-insper <viniciusbf1@al.insper.edu.br>
2016-05-04 17:24:30 -0300

    reorganizei a classe

--- Banco de Dados.py
+++ Banco de Dados.py
@@ -1,7 +1,19 @@
-class Banco de Dados():
+class Menu:
 	def __init__(self):
-		self.menu = {'000':{'Coca Cola':3,50}, '001':{'Guarana':3,50}, '002':{'Citrus':3,50}, '003':{'Agua':3,00}, '004':{'Sucos':4,50},
-		'005':{'Batata Frita':8,00}, '006':{'Mandioca':9,00}, '007':{'Polenta':8,50}, '008':{'X-Burguer':9,50}, '009':{'Burguer':9,00}, 
-		'010':{'X-Frango':10,50}, '011':{'Frango':10,00}, '012':{'Ovo':1,50}, '013':{'Bacon':1,50}, '014':{'Salada':1,50}, '015':{'MilkShake':6,50}}
-
-
+		self.menu_codigos = {}
+		self.menu_codigos['000'] = {'Coca Cola': 3.50}
+		self.menu_codigos['001'] = {'Guarana': 3.50}
+		self.menu_codigos['002'] = {'Citrus': 3.50}
+		self.menu_codigos['003'] = {'Agua': 3.50}
+		self.menu_codigos['004'] = {'Sucos': 3.50}
+		self.menu_codigos['005'] = {'Batata Frita': 3.50}
+		self.menu_codigos['006'] = {'Polenta Frita': 3.50}
+		self.menu_codigos['007'] = {'Mandioca Frita': 3.50}
+		self.menu_codigos['008'] = {'Burguer': 3.50}
+		self.menu_codigos['009'] = {'X-Burguer': 3.50}
+		self.menu_codigos['010'] = {'Frango': 3.50}
+		self.menu_codigos['011'] = {'X-Frango': 3.50}
+		self.menu_codigos['012'] = {'Ovo': 3.50}
+		self.menu_codigos['013'] = {'Bacon': 3.50}
+		self.menu_codigos['014'] = {'Salada': 3.50}
+		self.menu_codigos['015'] = {'MilkShake': 3.50}
