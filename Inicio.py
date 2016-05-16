be4f33e5b0ae67cab245f2d4fabab2aabcc35e07
woskley <wesleybau@hotmail.com>
2016-05-16 15:49:49 -0300

    Add files via upload

--- Inicio.py
+++ Inicio.py
@@ -4,25 +4,31 @@ import tkinter as tk
 
 class inicio :
     
-    def __init__ (self,toplevel):
-        #cria a interface do tkinter 
-        self.frame = tk.Frame(toplevel, width=500, height=500, bd=175)
+    def __init__ (self,master):
+        #cria a interface do tkinter
+        self.frame = tk.Frame(master, width=500, height=500, bd=175)
         self.frame.pack()
         
         #cria um botao para ser direcionado ao mapa
         self.botaomapa = tk.Button(self.frame,text ='Mapa',height=8 ,width= 20 ,bd=20,padx=20)
         self.botaomapa.pack(side=tk.LEFT)
+        self.botaomapa.configure(command = lambda: self.mapa())
         
         #cria uma botao para ser direcionado a cozinha 
         self.botaocozinha = tk.Button(self.frame,text ='Cozinha',height=8 ,width=20 ,bd=20,padx=20)
         self.botaocozinha.pack(side=tk.RIGHT)
-        self.botaocozinha.configure(command = lambda:self.cozinha())
+        self.botaocozinha.configure(command = lambda: self.cozinha())
         
         
-    def cozinha(self):
-        import GUI_cozinha
-        GUI_cozinha(window)
-        window.mainloop()
+        def cozinha(self):
+            import GUI_cozinha
+            GUI_cozinha(window)
+            window.mainloop()
+                
+        def mapa(self):
+            import tkmapa
+            tkmapa(window)
+            window.mainloop()
             
 
         
@@ -31,4 +37,5 @@ window=tk.Tk()
 window.title ('Inicio')
 inicio(window)
 window.mainloop()
+
 
