cf21a914921f0e0c35b2d522975d090b5e23428c
brunop2702 <brunop2702@hotmail.com>
2016-05-23 15:59:05 -0300

    função iterar feita

--- Pedidos.py
+++ Pedidos.py
@@ -1,4 +1,3 @@
-from BancodeDados import cMenu
 from firebase import firebase
 codigos = cMenu()
 firebase = firebase.FirebaseApplication('https://smartserver.firebaseio.com/')
@@ -20,3 +19,16 @@ class cPedido():
                     y = e
                 pedido_ = [hora, status, x, y]
                 firebase.put(url = 'mesa{0}/cadeira{1}'.format(self.mesa, self.cadeira), name = 'pedido numero {0}'.format(n_pedido), data = pedido_)
+                
+    def iterar(self):
+        lista='CADEIRA {0} \n'.format(self.cadeira)
+        soma = 0 
+        x= firebase.get(url='/mesa{0}/'.format(self.mesa),name= 'cadeira{0}'.format(self.cadeira))
+        for i in x.values():
+            if i[2] != 'produto':
+                lista+=i[2]+str(i[3])+'\n'
+                j= i[3]
+                soma+=(j)
+        lista+='total = '+str(soma)+'\n'
+        lista+='c/ serviço = '+str("{0:.2f}".format(soma*1.1))
+        return [lista,soma*1.1] 
